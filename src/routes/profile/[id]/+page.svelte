4c1fe04701907d6fe578810b64edb45bf421384e
Ashures <ashuresyt@gmail.com>
2023-10-30 21:36:44 +1300

    Added HTML to display user info to /profile page

---
+++ src/routes/profile/[id]/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let data;
+
+  const user = data.results;
+</script>
+
+<h1>{user.username}</h1>
+
+<ul>
+  <li>Name: {user.firstName} {user.lastName}</li>
+  <li>Email: {user.emailAddress}</li>
+</ul>
