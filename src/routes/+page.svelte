0b7ad55d3286371c7e9dfa999251c4f5e3bfc8aa
Ashures <ashuresyt@gmail.com>
2023-10-30 22:12:58 +1300

    Added register form to index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
 <h1><a href="/profile/1">Profile 1</a></h1>
 <h1><a href="/profile/2">Profile 2</a></h1>
+
+<form method="POST" action="?/register">
+  <input name="firstName" type="text" placeholder="First Name">
+  <input name="lastName" type="text" placeholder="Last Name">
+  <input name="emailAddress" type="email"  placeholder="Email Address">
+  <input name="username" type="text" placeholder="Username">
+  <input name="password" type="password" placeholder="Password">
+  <button>Register</button>
+</form>
