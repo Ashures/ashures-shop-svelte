e1dafbe1efc1ea8e4e366f5ca5e1f56321a0b3a8
wilsjw <wilsjw2@student.op.ac.nz>
2023-11-02 10:41:08 +1300

    Added axios post request to sendAPI

--- src/lib/utils/sendAPI.js
+++ src/lib/utils/sendAPI.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 
-const sendAPI = async () => {
+const sendAPI = async (url, data) => {
+    try {
+        const res = await axios.post(url, data);
 
+        console.log(res);
+    } catch (error) {
+        console.log(error);
+    }
+        
 };
 
 export default sendAPI;
