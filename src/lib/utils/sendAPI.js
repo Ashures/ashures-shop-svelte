9187d43b3f303f2a92757a3a29946b6fe755c2ec
wilsjw <wilsjw2@student.op.ac.nz>
2023-11-02 10:56:09 +1300

    Removed debugging statements from sendAPI

--- src/lib/utils/sendAPI.js
+++ src/lib/utils/sendAPI.js
@@ -3,8 +3,6 @@ import axios from "axios";
 const sendAPI = async (url, data) => {
     try {
         const res = await axios.post(url, data);
-
-        console.log(res);
     } catch (error) {
         console.log(error);
     }
