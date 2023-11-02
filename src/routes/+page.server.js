9d117b2faf7400d01a18f0b3291e37b3b5ddf4bf
Ashures <ashuresyt@gmail.com>
2023-11-02 15:48:50 +1300

    Refactored page actions with formDataToJson

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,23 +1,13 @@
 import getAPI from "$lib/utils/getAPI.js";
-import sendAPI from '$lib/utils/sendAPI.js'
-import { send } from "vite";
+import sendAPI from '$lib/utils/sendAPI.js';
+import formDataToJson from '$lib/utils/formDataToJson.js';
 
 export const actions = {
   login: async ({ request }) => {
-    const data = await request.formData();
-    let user = {};
-
-    data.forEach((value, key) => {
-      user[key] = value;
-    });
+    const user = await formDataToJson(request);
   },
   register: async ({ request }) => {
-    const data = await request.formData();
-    let user = {};
-
-    data.forEach((value, key) => {
-      user[key] = value;
-    });
+    const user = await formDataToJson(request);
     
     await sendAPI(`https://ashures-shop.onrender.com/api/users/`, user);
   }
