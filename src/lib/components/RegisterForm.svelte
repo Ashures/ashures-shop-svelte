d97bd6583af17d3da749705461b371f942b2be1f
Ashures <ashuresyt@gmail.com>
2023-11-02 15:33:35 +1300

    Moved form from index to RegisterForm

---
+++ src/lib/components/RegisterForm.svelte
@@ -0,0 +1,8 @@
+<form method="POST" action="?/register">
+  <input name="firstName" type="text" placeholder="First Name">
+  <input name="lastName" type="text" placeholder="Last Name">
+  <input name="emailAddress" type="email"  placeholder="Email Address">
+  <input name="username" type="text" placeholder="Username">
+  <input name="password" type="password" placeholder="Password">
+  <button>Register</button>
+</form>
--- src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<form method="POST" action="?/register">
-  <input name="firstName" type="text" placeholder="First Name">
-  <input name="lastName" type="text" placeholder="Last Name">
-  <input name="emailAddress" type="email"  placeholder="Email Address">
-  <input name="username" type="text" placeholder="Username">
-  <input name="password" type="password" placeholder="Password">
-  <button>Register</button>
-</form>
