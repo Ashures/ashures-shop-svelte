7e1fe621aebde63859411a279aa37c0bf0cc89a3
Ashures <ashuresyt@gmail.com>
2023-11-02 16:06:09 +1300

    Added tabs list with each block showing each tab

--- src/lib/components/TabMenu.svelte
+++ src/lib/components/TabMenu.svelte
@@ -1,8 +1,16 @@
+<script>
+  export let tabs;
+
+  let currentTab = 0;
+</script>
+
 <div class="tab-container">
   <div class="tab-list">
-
+    {#each tabs as tab, i (tab.name)}
+      <button class={i === currentTab ? "tab selected-tab" : "tab"}>{tab.name}</button>
+    {/each}
   </div>
   <div class="tab-content">
-    
+    <svelte:component this={tab[currentTab].component} />
   </div>
 </div>
