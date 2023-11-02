8c2b4ad4bf8ea0c3a6ab862123055d71af2cc23f
Ashures <ashuresyt@gmail.com>
2023-11-02 15:32:46 +1300

    Removed unneeded profile links from index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,3 @@
-<h1><a href="/profile/1">Profile 1</a></h1>
-<h1><a href="/profile/2">Profile 2</a></h1>
-
 <form method="POST" action="?/register">
   <input name="firstName" type="text" placeholder="First Name">
   <input name="lastName" type="text" placeholder="Last Name">
