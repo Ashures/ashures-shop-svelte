c5a2be3dcaf9897deeb287c367cd446280ac07b6
Ashures <ashuresyt@gmail.com>
2023-11-02 15:35:07 +1300

    Added form to LoginForm

---
+++ src/lib/components/LoginForm.svelte
@@ -0,0 +1,5 @@
+<form method="POST" action="?/login">
+  <input name="username" type="text" placeholder="Username">
+  <input name="password" type="password" placeholder="Password">
+  <button>Login</button>
+</form>
