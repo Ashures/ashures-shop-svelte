7256b8d909e232f62a1d434ffd6df3bc20c32fbb
Ashures <ashuresyt@gmail.com>
2023-11-02 16:11:34 +1300

    Wrote code for changing tabs in TabMenu

--- src/lib/components/TabMenu.svelte
+++ src/lib/components/TabMenu.svelte
@@ -1,13 +1,28 @@
 <script>
+  import { onMount } from "svelte";
+
   export let tabs;
 
+  let tabButtons = [];
   let currentTab = 0;
+
+  const changeTab = (index) => {
+    if (currentTab === index) return;
+
+    tabButtons[currentTab].classList.remove("selected-tab");
+    currentTab = index;
+    tabButtons[currentTab].classList.add("selected-tab");
+  };
+
+  onMount(() => {
+    tabButtons = document.getElementsByClassName("tab");
+  });
 </script>
 
 <div class="tab-container">
   <div class="tab-list">
     {#each tabs as tab, i (tab.name)}
-      <button class={i === currentTab ? "tab selected-tab" : "tab"}>{tab.name}</button>
+      <button class={i === currentTab ? "tab selected-tab" : "tab"} on:click={() => changeTab(i)}>{tab.name}</button>
     {/each}
   </div>
   <div class="tab-content">
