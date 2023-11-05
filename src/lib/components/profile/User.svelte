aa70b4fa02e6898d336213037c020f13fe868f2a
Ashures <ashuresyt@gmail.com>
2023-11-05 20:46:39 +1300

    Added User component

---
+++ src/lib/components/profile/User.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let user;
+</script>
+
+<div class="user-container">
+  <img src={user.icon} alt="User icon" class="user-icon">
+  <div class="user-info">
+    <h1>{user.username}</h1>
+    <ul>
+      <li>Name: {user.firstName} {user.lastName}</li>
+      <li>Email: {user.emailAddress}</li>
+    </ul>
+  </div>
+</div>
